95ac88e10ef4c04badea2c51aaa4be1e4228cf0f
Rafael Rocha <rafael.rcavalcanti@dataprev.gov.br>
2023-02-17 22:03:48 -0300

    - simples + map

--- src/test/java/com/example/rxjava/ObservablesTest.java
+++ src/test/java/com/example/rxjava/ObservablesTest.java
@@ -3,7 +3,10 @@ package com.example.rxjava;
 import io.reactivex.rxjava3.core.Observable;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class ObservablesTest {
@@ -14,6 +17,18 @@ public class ObservablesTest {
         Observable<String> observable = Observable.just("hello");
         observable.subscribe(s -> result.set(s) );
         Assertions.assertEquals("hello", result.get());
+    }
+
+    @Test
+    public void map() {
+        AtomicReference<List<Integer>> result = new AtomicReference<>(new ArrayList<>());
+        Observable<Integer> observable = Observable.just(1, 3, 5, 7, 11);
+        observable
+            .filter(k -> k % 2 == 1)
+            .map(a -> a * 2)
+            .subscribe( n -> result.get().add(n) );
+
+        Assertions.assertEquals(List.of(2, 6, 10, 14, 22), result.get());
     }
 
 }
